e7bd533163eb547bc10030091c1138ffeb24841d
Per Rawdin <perrawd@gmail>
2023-02-19 17:08:53 +0100

    Add basic layout and styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,26 @@
 'use client';
 
+import { Roboto_Mono } from '@next/font/google';
 import { Container } from '@mantine/core';
 import RootStyleRegistry from './emotion';
+import './styles.css';
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+const roboto = Roboto_Mono({
+  weight: '200',
+  subsets: ['latin'],
+});
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en-US">
       <head />
-      <body>
+      <body className={roboto.className}>
         <RootStyleRegistry>
-          <Container>
-            {children}
-          </Container>
+          <Container>{children}</Container>
         </RootStyleRegistry>
       </body>
     </html>
