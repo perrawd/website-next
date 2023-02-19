cc67cd68e62a569a230a69727f7760f189d06f88
Per Rawdin <perrawd@gmail>
2023-02-19 17:09:59 +0100

    Add draft nord layout header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,28 @@
 'use client';
 
+import { ActionIcon } from '@mantine/core';
+import {
+  IconBrandLinkedin, IconMail, IconPhone, IconBrandGithub,
+} from '@tabler/icons-react';
+
 export default function Page() {
   return (
-    <h1>Hello World</h1>
+    <div>
+      <h1>Per Rawdin</h1>
+      <div style={{ display: 'flex' }}>
+        <ActionIcon variant="outline" size="lg" style={{ marginRight: '7px' }} aria-label="GitHub">
+          <IconBrandGithub />
+        </ActionIcon>
+        <ActionIcon variant="outline" size="lg" style={{ marginRight: '7px' }} aria-label="LinkedIn" on>
+          <IconBrandLinkedin />
+        </ActionIcon>
+        <ActionIcon variant="outline" size="lg" style={{ marginRight: '7px' }} aria-label="E-mail">
+          <IconMail />
+        </ActionIcon>
+        <ActionIcon variant="outline" size="lg" style={{ marginRight: '7px' }} aria-label="Phone">
+          <IconPhone />
+        </ActionIcon>
+      </div>
+    </div>
   );
 }
